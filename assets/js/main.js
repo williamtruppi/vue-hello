b27109d62e6e9172a07b040b0e86c677045ddd66
William Truppi <williamt@hotmail.it>
2020-12-14 14:13:38 +0100

    esercizio completato con bonus

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/* Istruzioni:
+Stampare a schermo un messaggio all’interno di un h1, 
+utilizzando i data.
+Bonus: Aggiungere un’immagine presa anch’essa da un data */
+
+let app = new Vue ({
+
+  el: "#root",
+
+  data: {
+    msg: "Viva Las Vegas",
+    img: "assets/img/greggio.jpg"
+  }
+})
